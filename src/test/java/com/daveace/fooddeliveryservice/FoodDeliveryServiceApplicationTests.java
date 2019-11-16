9f25b9bebd541aa8e04a3766879f46deda940616
richard ekong <ekong.richard@yahoo.com>
2019-11-16 21:43:43 +0100

    Add initial project files

---
+++ src/test/java/com/daveace/fooddeliveryservice/FoodDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daveace.fooddeliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
